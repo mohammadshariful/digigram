4f42ff31de03565dd7273e9cb2ef3e0051465392
Mohammad-Shariful <mohammadsharifulislam64@gmail.com>
2022-01-08 16:06:37 +0600

    project already done

--- app/main.js
+++ app/main.js
@@ -7,6 +7,7 @@ const closeMenu = document.querySelector(".fa-times");
 // functionalities
 
 //for-open Menu
+/*
 openMenu.addEventListener("click", function () {
   openMenu.style.display = "none";
   closeMenu.style.display = "block";
@@ -18,4 +19,43 @@ closeMenu.addEventListener("click", function () {
   closeMenu.style.display = "none";
   openMenu.style.display = "block";
   navLinks.style.right = "-200px";
+});
+*/
+// toggle function
+function toggle(stateNone, pixel, stateBlock) {
+  openMenu.style.display = stateNone;
+  closeMenu.style.display = stateBlock;
+  navLinks.style.right = pixel;
+}
+
+openMenu.addEventListener("click", () => toggle("none", 0, "block"));
+
+closeMenu.addEventListener("click", () => toggle("block", "-200px", "none"));
+
+/*
+function toggle(state, size) {
+  state === "block"
+    ? (openMenu.style.display = state)
+    : (closeMenu.display = "none");
+  navLinks.style.right = size + "px";
+}
+*/
+
+//counters
+const counters = document.querySelectorAll(".count");
+counters.forEach((counter) => {
+  counter.innerText = "0";
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const increase = target / 2000;
+
+    const c = +counter.innerText;
+    if (c < target) {
+      counter.innerText = Math.ceil(c + increase);
+      setInterval(updateCounter, 1);
+    } else {
+      counter.innerText = target;
+    }
+  };
+  updateCounter();
 });
